f95fcb302b4ad8dc40f235d1204430d12428abf1
Arjuna Ravikumar <aravikumar@ea.com>
2022-03-02 17:41:17 -0600

    Adding video capture to project

--- sierpinski.js
+++ sierpinski.js
@@ -1,11 +1,43 @@
-var pal = []
-var cnv
+var prevmid = 0.5
+var rot = 0
+var minskew = 0.00
+
+var inc = 1/360.0/2.0
+var rotspeed = 1/2.0
+// var inc = 0
+// var rotspeed = 0
+
+var scl = 0.8
+
+var enableCapture = false
+var captureStart = 1
+var captureEnd = 5
+var _isCapturing = false
+
+function startCapture(){
+    if(!enableCapture) return
+    if(frameCount == captureStart){
+        _isCapturing = true
+        capturer.start()
+    }
+}
+
+function doCapture() {
+    if(_isCapturing){
+        capturer.capture(canvas)
+        if(frameCount === captureEnd){
+            capturer.stop()
+            capturer.save()
+        }
+    }
+}
+
+
 
 function sierpinski(left, right, top, iterations, midpoint, palette, altpallete, index){
 
     if(iterations == 0)
-    {
-        
+    {        
         fill(palette[index])
         noStroke()
         triangle(left.x, left.y, right.x, right.y, top.x, top.y)
@@ -15,8 +47,6 @@ function sierpinski(left, right, top, iterations, midpoint, palette, altpallete,
     var basemid = p5.Vector.lerp(left, right, midpoint)
     var rightmid = p5.Vector.lerp(right, top, midpoint)
     var leftmid = p5.Vector.lerp(top, left, midpoint)
-    
-    pal = blues
 
     sierpinski(left, basemid, leftmid, iterations - 1, midpoint, palette, altpallete, 2)
     sierpinski(basemid, right, rightmid, iterations - 1, midpoint, palette, altpallete, 4)
@@ -24,38 +54,44 @@ function sierpinski(left, right, top, iterations, midpoint, palette, altpallete,
     sierpinski(leftmid, basemid, rightmid, iterations - 1, .5, altpallete, altpallete, 8)
 }
 
-var prevmid = 0.5
-var rot = 0
-var minskew = 0.00
 
-var inc = 0.001
-var rotspeed = 0.1
-// var inc = 0
-// var rotspeed = 0
-
-var scl = 0.8
 
 function keyTyped(){
     if(key === "s"){
-        save(cnv, "sierpinski.png")
+        save(canvas, "sierpinski.png")
+    }
+
+    if (key == "v"){
+        capturer.start()
+    }
+
+    if(key ==="x"){
+        capturer.stop()
+        capturer.save()        
     }
 }
 
 function setup(){
+    
     angleMode(DEGREES)
-    // pal = pinks
-    cnv = createCanvas(800, 800)
+    createCanvas(800, 800)
 }
 
 function draw(){
+
+    startCapture()
+    
+    
     background(pinks[4])
 
-    // scl*=1.001
     var l = width*scl
     var h = l * sin(60)
-    var cy = l/2 * tan(30)
-    var cyy = h - cy
-    var r = (l/2) / cos(30)
+    
+    var left = -l/2
+    var right = l/2
+    var midx = 0
+    var top = - h* 2/3
+    var bottom = h * 1/3
 
     var midpoint = prevmid + inc
     if(midpoint>1-minskew || midpoint < minskew){        
@@ -73,11 +109,14 @@ function draw(){
     rotate(rot)
     
     sierpinski(
-        createVector(-l/2, cy), 
-        createVector(l/2, cy), 
-        createVector(0, -cyy), 5, midpoint, blues, pinks, 0)
+        createVector(left, bottom), 
+        createVector(right, bottom), 
+        createVector(midx, top),
+        5, midpoint, blues, pinks, 0)
 
     prevmid = midpoint
 
     pop()
+
+    doCapture()
 }
